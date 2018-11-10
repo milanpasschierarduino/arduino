837831d5716047a823758f6f8da8df91f7edc78b
milanarduino <milanarduino@gmail.com>
2018-11-10 14:22:30 -0500

    test twee voor database

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,21 @@ var express = require('express');
 var router = express.Router();
 const db = require('monk')('mongodb://milanarduino:detering1@ds159293.mlab.com:59293/milanarduino')
 const sessions = db.get('sessions')
+var dateFormat = require('dateformat');
+var randomstring = require("randomstring");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/session/new', function(req, res, next) {
+router.get('/session/new', function(req, res, next) {
   
-  console.log(req.body);
-  sessions.insert({ name: 'Tobi' });
+  var now = new Date();
+  var date = dateFormat(now, "dd, mm, yyyy");
+  var id = randomstring.generate(10);
   
+  sessions.insert({ status: 'inactive', date: date, id: id });
   res.send('ontvangen');
   
 });
