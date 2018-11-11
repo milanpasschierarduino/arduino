1d20c9616d90e2cbe7aee40ff432ee8b9143546c
milanarduino <milanarduino@gmail.com>
2018-11-11 06:52:58 -0500

    test zes voor database

--- routes/index.js
+++ routes/index.js
@@ -43,9 +43,19 @@ router.get('/session/find', function(req, res, next) {
   var now = new Date();
   var date = dateFormat(now, "dd-mm-yyyy");
   
-  sessions.findOne({ date: date, status: "inactive" }).then(function (doc) {
+  sessions.findOne({ date: date }).then(function (doc) {
     
     res.json(doc);
+    
+  })
+  
+});
+
+router.get('/session/start', function(req, res, next) {
+  
+  sessions.findOneAndUpdate({ date: req.query.date, id: req.query.id}, {$set: {status: "active"}}).then((updated) => {
+    
+    res.json(updated);
     
   })
   
